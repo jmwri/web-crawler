91ca2bed6e30ebc7db02c9a1e77479231964a42a
Jim <jmwri93@gmail.com>
2021-04-25 21:13:58 +0100

    Set up default public crawler

--- crawler.go
+++ crawler.go
@@ -1,6 +1,8 @@
 package web_crawler
 
 import (
+	"github.com/jmwri/web-crawler/internal"
+	"net/http"
 	"net/url"
 )
 
@@ -20,4 +22,27 @@ type Result interface {
 	MaxDepth() int
 	// URLs returns a map of URLs that the crawler visited, and a list of URLs found on that page
 	URLs() map[string][]string
+}
+
+// DefaultCrawler is the default Crawler
+var DefaultCrawler Crawler
+
+func init() {
+	loader := internal.NewHTTPGetLoader(http.DefaultClient)
+	DefaultCrawler = crawler{
+		loader:    internal.LoaderWithRetry(loader.Load, internal.SimpleBackoff, 5),
+		extractor: internal.HtmlTokenExtractor,
+	}
+}
+
+// crawler is a wrapper around the internal Crawler. It means we can use public interfaces.
+type crawler struct {
+	loader    internal.LoaderFunc
+	extractor internal.ExtractorFunc
+}
+
+// Crawl according to the specified options
+func (c crawler) Crawl(target *url.URL, sameDomain bool, maxDepth int, workers int) (Result, error) {
+	o := internal.NewCrawlOptions(target, sameDomain, maxDepth, workers)
+	return internal.Crawl(c.loader, c.extractor, o)
 }
